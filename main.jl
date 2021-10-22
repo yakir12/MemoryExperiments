b805604b5d82758022a99fcdcdb8e44e72b4bd8e
Yakir Gagnon <12.yakir@gmail.com>
2021-10-22 15:44:46 +0200

    final

--- main.jl
+++ main.jl
@@ -6,12 +6,12 @@
 # Pkg.instantiate()
 
 # packages we're using
-using Arrow, DataFrames, GLMakie, Statistics, LinearAlgebra, OnlineStats, CSV, Dates, Chain, Rotations, CoordinateTransformations, StatsBase, Colors, StaticArrays, Missings, StructArrays, IntervalSets, Interpolations, CategoricalArrays
+using Arrow, DataFrames, CairoMakie, Statistics, LinearAlgebra, OnlineStats, CSV, Dates, Chain, Rotations, CoordinateTransformations, StatsBase, Colors, StaticArrays, Missings, StructArrays, IntervalSets, Interpolations, CategoricalArrays
 import IterTools:partition
 
 include("plots.jl")
 include("utils.jl")
-#
+
 link = "/home/yakir/downloads/db.arrow"
 df = @chain link begin
   # download # download the data
@@ -19,10 +19,11 @@ df = @chain link begin
   DataFrame # transform to a dataframe
   transform(
             [:dropoff, :fictive_nest, :nest] .=> ByRow(passmissing(Point2f0)) .=> [:dropoff, :fictive_nest, :nest],
-            [:t, :tp] => ByRow((t, tp) -> ceil(Int, t[tp])) => :tp,
-            :rawcoords => ByRow(filter_jump) => :coords,
+            # [:t, :tp] => ByRow((t, tp) -> ceil(Int, t[tp])) => :tp,
+            # :rawcoords => ByRow(filter_jump) => :coords,
+            :coords => ByRow(x -> Point2f0.(x)) => :coords,
             [:Experiment, :treatment] => ByRow(to_figure) => :figure, # what figure
-            :treatment => ByRow(x -> get(treatments, x, x)) => :treatment
+            :treatment => ByRow(treatments) => :treatment
            )
   transform(
             [:coords, :tp] => ByRow((xy, i) -> xy[1:i]) => :homing, # the homing part of the track
@@ -40,67 +41,20 @@ mkpath(path)
 colors = distinguishable_colors(length(unique(df.figure)), [colorant"white", colorant"black"], dropseed = true)
 for ((k, gd), color) in zip(pairs(groupby(df, :figure, sort = true)), colors)
   fig = plottracks(gd; color)
-  save(joinpath(path, string("track ", k..., ".png")), fig)
+  save(joinpath(path, string("track ", k..., ".pdf")), fig)
 end
 
 
 ds = select(df, [:homing, :turning_point, :searching] => ByRow(get_speed) => ["$i" for i in [-reverse(speed_mid); speed_mid]], :figure, :treatment)
-dd = stack(ds, Not(Cols(:figure, :treatment)))
+dd = stack(ds, Not(Cols(:figure, :treatment)), variable_name=:distance, value_name=:speed)
 rm2small!(dd)
 dropmissing!(dd)
-transform!(dd, :variable => x -> parse.(Int, x); renamecols = false)
+transform!(dd, :distance => x -> parse.(Int, x); renamecols = false)
 
-<<<<<<< HEAD
-M = maximum(abs, dd.variable) + speed_interval
+Mx = maximum(abs, dd.distance) + speed_interval
+My = maximum(dd.speed)
 for ((k, gd), color) in zip(pairs(groupby(dd, :figure, sort = true)), colors)
-  fig = plotspeeds(gd, M; color)
-  save(joinpath(path, string("speed ", k..., ".png")), fig)
-=======
-df = @chain link begin
-  download # download the data
-  Arrow.Table # transform to an arrow table
-  DataFrame # transform to a dataframe
-  filter(:tp => >(1), _) # we can't use any tracks with too-few coordinates
-  sort("holding time") # sort by holding time
-  transform( # transform to coordinate types
-            :coords => ByRow(xy -> Point2f0.(xy)) => :coords,
-            [:dropoff, :fictive_nest] .=> ByRow(Point2f0) .=> [:dropoff, :fictive_nest]
-           )
-  # center everything on the dropoff point
-  transform([:coords, :dropoff] => ByRow(.-) => :coords,
-            [:fictive_nest, :dropoff] => ByRow(-) => :fictive_nest,
-            :dropoff => ByRow(zero) => :dropoff,
-           )
-  # apply a couple of convenient transformations
-  transform([:coords, :tp] => ByRow((xy, i) -> xy[1:i]) => :homing, # keep only the homing part of the track
-            [:coords, :tp] => ByRow((xy, i) -> xy[i]) => :turning_point, # add the turning point
-            :date => ByRow(x -> Date(string(x), dateformat"yyyymmdd")) => :date, # change the date to a real date type
-            ["feeder to nest", "holding condition"] => ByRow(categorize4figure) => :figure # add a categorization for the figure
-           )
-  # some stats
-  transform([:homing, :t] => ByRow(speedstats) => "speed μ±σ", 
-            [:dropoff, :turning_point] => ByRow(norm ∘ -) => "vector length",
-            :homing => ByRow(xy -> sum(norm, diff(xy))) => "path length",
-            [:nest2feeder, :turning_point] => ByRow((n2f, tp) -> -n2f - last(tp)) => "nest corrected vector",
-            [:dropoff, :turning_point] => ByRow((a,b) -> rad2deg(abs(angular_diff_from_pos_y_axis(b - a)))) => "angular difference",
-           )
-  @aside @chain _ begin
-    select(:runid,
-           "beetle ID",
-           :date,
-           "feeder to nest", 
-           "holding condition",
-           "holding time",
-           "speed μ±σ", 
-           "vector length",
-           "path length",
-           "nest corrected vector",
-           # "angular difference",
-          )
-    CSV.write(joinpath(path, "stats.csv"), _)
-  end
-  groupby(["holding condition", "feeder to nest", "holding time"])
-  transform(["holding condition", "feeder to nest"] => tocolors => :color)
->>>>>>> 89b091e29a08681b4512a6d0b7bef2a77ca00097
+  fig = plotspeeds(gd, Mx, My; color)
+  save(joinpath(path, string("speed ", k..., ".pdf")), fig)
 end
 
--- plots.jl
+++ plots.jl
@@ -1,26 +1,21 @@
 styles = (
           homing        = (label = "homing", linewidth = 2,),
           searching     = (label = "searching", linewidth = 1,),
-          turning_point = (label = "turning point", marker    = '◎',),
+          turning_point = (label = "turning point", marker    = '◎', color = :red),
           dropoff       = (label = "drop-off", marker    = '•', color = :white, strokewidth = 1),
           nest          = (label = "nest", marker    = :star5,),
           fictive_nest  = (label = "fictive nest", marker = :star5, color = :white, strokewidth = 1),
          )
 
 function plottrack(ax, dropoff, homing, turning_point, searching, fictive_nest, figure, color)
-  # lines!(ax, collect(Missings.replace(homing, Point2f(NaN, NaN))); color, styles.homing...)
   lines!(ax, homing; color, styles.homing...)
   if figure ≠ :conflict
-    # lines!(ax, collect(Missings.replace(searching, Point2f(NaN, NaN))); color, styles.searching...)
     lines!(ax, searching; color, styles.searching...)
   end
   if figure == :displacement
     scatter!(ax, dropoff; strokecolor = color, styles.dropoff...)
   end
   scatter!(ax, turning_point; color, styles.turning_point...)
-  # if figure == :transfer
-  #   scatter!(ax, fictive_nest; color, styles.fictive_nest...)
-  # end
 end
 
 function plottracks(ax, dropoff, homing, turning_point, searching, fictive_nest, nest, figure, color)
@@ -47,17 +42,40 @@ function plottracks(df; color = :black)
   fig
 end
 
-function plotspeeds(df, M; color = :black)
+function plotspeeds(df, Mx, My; color = :black)
   fig = Figure()
   for (i, (k, gd)) in enumerate(pairs(groupby(df, :treatment)))
     ax = Axis(fig[1,i], aspect = 1, title = string(k...), ylabel = "Speed (cm/sec)")
-    boxplot!(ax, gd.variable, color = color, gd.value, width=5, show_median=true, show_outliers = false)
+    boxplot!(ax, gd.distance, color = color, gd.speed, width=speed_interval/2, show_median=true, show_outliers = false)
     vlines!(ax, 0, color = :gray)
   end
   axs = contents(fig[1, :])
   linkaxes!(axs...)
-  xlims!(axs[1], -M, M)
+  xlims!(axs[1], -Mx, Mx)
+  ylims!(axs[1], nothing, My)
   hideydecorations!.(axs[2:end], grid = false)
   Label(fig[2,:], "Radial distance from the turning point (cm)", tellwidth = false, tellheight = true)
   fig
 end
+
+function ploteach()
+  fig = Figure()
+  sl = Slider(fig[2, 1], range = 1:nrow(df))
+  homing = lift(sl.value) do i
+    Vector{Union{Missing, Point2f0}}(df.homing[i])
+  end
+  turning_point = lift(sl.value) do i
+    df.turning_point[i]
+  end
+  searching = lift(sl.value) do i
+    Vector{Union{Missing, Point2f0}}(df.searching[i])
+  end
+  ax = Axis(fig[1,1], aspect = DataAspect())
+  lines!(ax, homing; styles.homing...)
+  scatter!(ax, turning_point; styles.turning_point...)
+  lines!(ax, searching; styles.searching...)
+  fig
+end
+using GLMakie
+GLMakie.activate!()
+ploteach()
